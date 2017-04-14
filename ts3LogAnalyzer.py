632e403c1c52c25f8e0ecd17242162c92713ea67
Francisco Marques <me@tofran.com>
2017-04-14 23:59:24 +0100

    Added parsing

--- ts3LogAnalyzer.py
+++ ts3LogAnalyzer.py
@@ -1,30 +1,40 @@
+#!/usr/bin/python
 """
 ts3LogAnalyzer.py
 
 Usage:
-    ts3LogAnalyzer.py -l <path> [-d <path>]
+    ts3LogAnalyzer.py <log> [-d <path>]
 	ts3LogAnalyzer.py --version
 
 Options:
-    -l --logs <path>         Log file or folder to analyze
     -d --database <path>        Database to use or create
     -h --help		            Show this screen
 	--version	               	Show version
 """
 
+__author__ = 'ToFran'
+__site__ = 'http://tofran.com/'
+
+__version__ = "2.0"
+__maintainer__ = "ToFran"
+__email__ = "me@tofran.com"
+__license__ = "GNU GPLv3"
+
 import os
+import sys
 import logging
 import sqlite3
 import glob
 from docopt import docopt
+
 
 cnn = None
 
 def main():
     global cnn
     arguments = docopt(__doc__, version='2.0')
-    logging.basicConfig(level=logging.DEBUG)
-    print(arguments)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.debug('arguments: ' + str(arguments))
 
     #if no databse parameter, use databse.db
     database = 'database.db'
@@ -41,16 +51,21 @@ def main():
             logging.warning('-d not specified, using ' + database)
 
     #log files
-    path = arguments['--logs']
+    path = arguments['<log>']
     if os.path.isdir(path):
-        for f in glob.glob("*_1.log"):
-            loadFile(f)
+        logging.debug(path + ' is a folder.')
+        for f in glob.glob("*.log"):
+            analyseFile(f)
     elif os.path.isfile(path):
-        loadFile(path)
+        logging.debug(path + ' is a file.')
+        analyseFile(path)
+    else:
+        logging.critical(path + " does not exist! Terminating...")
+        sys.exit()
 
     #close db
-    c.commit()
-    c.close()
+    cnn.commit()
+    cnn.close()
 
 def create_db(cnn):
     with open('schema.sql', 'r') as f:
@@ -58,9 +73,43 @@ def create_db(cnn):
         cur = cnn.cursor()
         cur.executescript(sql)
 
-def loadFile(filepath):
+def analyseFile(filepath):
     print(filepath)
+    #slpitMessage("client connected 'user'(id:2) from 0.0.0.0:36634")
 
+    lineN = 0
+    lineArr = []
+    with open(filepath, 'r') as f:
+        for line in f:
+            if len(line.strip()) > 1:
+                lineN += 1
+                logging.debug('line' + str(lineN))
+                lineArr = line.strip().split('|')
+                message = slpitMessage(lineArr[4])
+                #time = datetime.datetime.strptime(lineArr[0], '%Y-%m-%d %H:%M:%S.%f')
+
+                if (lineArr[2] == 'VirtualServerBase' and
+                    len(message) >= 4 and message[0] == 'client' and
+                    (message[1] == 'connected' or message[1] == 'disconnected')):
+                        print(clientInfo[1] + ' ' + clientInfo[0])
+                        '''
+                        if message[1] == 'connected':
+                                clientConnected(time, clientInfo[1], clientInfo[0], message[-1].split(':')[0])
+                            elif message[1] == 'disconnected':
+                                clientDisconnected(time, clientInfo[1], clientInfo[0])
+                        '''
+
+
+def slpitMessage(message):
+    i = 0
+    arr = []
+    pos = message.index(' ')
+    while pos != -1:
+        arr.append(message[0:pos])
+        message = message[pos+1:]
+        pos = message.find(' ')
+    logging.debug('slpitMessage(' + message + ') = ' + str(arr))
+    return arr
 
 if __name__ == "__main__":
     main()
