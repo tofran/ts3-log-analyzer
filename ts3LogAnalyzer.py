94311c88e28ac24713165141e2767e4e6e174d24
Francisco Marques <kiko96kiko@gmail.com>
2017-04-17 03:33:40 +0100

    Added --hide-ip  and organized code

--- ts3LogAnalyzer.py
+++ ts3LogAnalyzer.py
@@ -3,22 +3,24 @@
 ts3LogAnalyzer.py
 
 Usage:
-    ts3LogAnalyzer.py <log> [-d <path>]
-    ts3LogAnalyzer.py --version
+    ts3LogAnalyzer.py <log> [-d <path>] [--hide-ip]
+    ts3LogAnalyzer.py -h | --help
+    ts3LogAnalyzer.py -v | --version
 
 Options:
-    -d --database <path>        Database to use or create
-    -h --help                   Show this screen
-	-v --version                Show version
+    -d --database <path>    Database to use or create (database.db used by default)
+    --hide-ip               Don't save ips
+    -h --help               Show this screen
+    -v --version            Show version
 """
 
 __author__ = 'ToFran'
 __site__ = 'http://tofran.com/'
 
-__version__ = "2.0"
-__maintainer__ = "ToFran"
-__email__ = "me@tofran.com"
-__license__ = "GNU GPLv3"
+__version__ = '2.0'
+__maintainer__ = 'ToFran'
+__email__ = 'me@tofran.com'
+__license__ = 'GNU GPLv3'
 
 import os
 import sys
@@ -28,14 +30,16 @@ import glob
 from docopt import docopt
 
 db = None
+hideIp = False
 openConn = dict()
 
 def main():
-    global db
+    global db, HIDEIP
     arguments = docopt(__doc__, version='2.0')
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
-    logging.debug('arguments: ' + str(arguments))
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG)
+    logging.debug("arguments: " + str(arguments))
 
+    hideIp = arguments['--hide-ip']
     #if no databse parameter, use databse.db
     database = 'database.db'
     if arguments['--database']:
@@ -48,16 +52,16 @@ def main():
         create_db(db)
         logging.info(database + ' created!')
     elif not arguments['--database']:
-            logging.warning('-d not specified, using ' + database)
+            logging.warning("-d not specified, using " + database)
 
     #log files
     path = arguments['<log>']
     if os.path.isdir(path):
-        logging.debug(path + ' is a folder.')
-        for f in glob.glob("*.log"):
+        logging.debug(path + " is a folder.")
+        for f in glob.glob('*.log'):
             analyseFile(f)
     elif os.path.isfile(path):
-        logging.debug(path + ' is a file.')
+        logging.debug(path + " is a file.")
         analyseFile(path)
     else:
         logging.critical(path + " does not exist! Terminating...")
@@ -67,14 +71,9 @@ def main():
     db.commit()
     db.close()
 
-def create_db(conection):
-    with open('schema.sql', 'r') as f:
-        schema = f.read()
-        cur = conection.cursor()
-        conection.executescript(schema)
-
 def analyseFile(filepath):
-    logging.info('Analyzing ' + filepath)
+    logging.info("Analyzing " + filepath)
+    insertLog(filepath)
     lineN = 0
     lineArr = []
     connection = dict()
@@ -95,9 +94,10 @@ def analyseFile(filepath):
                         elif message[1] == 'disconnected':
                             clientDisconnected(lineArr[0], getId(message[3]), message[2], getReason(message[5]))
 
-
+#################
+#PARSING
 def slpitMessage(message):
-    logging.debug('slpitMessage(' + message + '):')
+    logging.debug("slpitMessage(" + message + "):")
     message = message.strip()
     i = 0
     arr = []
@@ -121,14 +121,14 @@ def slpitMessage(message):
 
 def getId(string):
     string.strip()
-    if len(string) > 5 and string.startswith('(id:'):
+    if len(string) > 5 and string.startswith("(id:"):
         return int(string[4:-1])
     logging.error("Couln't parse ID from: " + string + " !")
     return -1
 
 def getIp(string):
     string.strip()
-    pos = string.find(":")
+    pos = string.find(':')
     if pos != -1:
         return string[0:pos]
     logging.error("Couln't parse IP from: " + string + " !")
@@ -136,22 +136,22 @@ def getIp(string):
 
 def getReason(string):
     string.strip()
-    pos = string.find("=")
+    pos = string.find('=')
     if pos != -1:
         return string[pos+1:]
     logging.error("Couln't get reason from: " + string + " !")
     return string
 
+#################
+#ACTIONS
 def clientConnected(when, id, nickname, ip):
+    logging.debug("ClientConnected(" + when + ", " + str(id) + ", " + nickname + ip)
     #check if user exist
     if not userExists(id):
-        #insert user
-        logging.debug("Creating new user " + str(id) + " (" + nickname + ")")
-        cur = db.cursor()
-        cur.execute("INSERT INTO user (id) VALUES (?)", [id])
+        insertUser(id)
 
+    nicknameUsed(id, nickname)
     #check if there is already a connecton opened
-    print (openConn)
     if id in openConn:
         openConn[id]['count'] += 1
     else:
@@ -159,20 +159,38 @@ def clientConnected(when, id, nickname, ip):
 
 def clientDisconnected(when, id, nickname, reason):
     if not id in openConn:
-        logging.error("Client dictonnected without starting connecton")
+        logging.error("Client dictonnected without starting connecton!")
         return False
 
     if openConn[id]['count'] > 1:
+        logging.debug("Closed 1 connection for " + str(id) + ", ramaining: " + str(openConn[id]['count']))
         openConn[id]['count'] -= 1
     else:
-        cur = db.cursor()
-        cur.execute("INSERT INTO connection (user, connected, disconnected, reason, ip) " + \
-                    "VALUES (?, ?, ?, ?, ?)", \
-                    [id, openConn[id]['connected'], when, reason, openConn[id]['ip']])
+        logging.debug('Client ' + str(id) + ' disconnected')
+        insertConnection(id, openConn[id]['connected'], when, reason, openConn[id]['ip'])
 
-def usedNickname(id, nickname):
+    nicknameUsed(id, nickname)
+    return True
+
+#################
+#DATABSE
+def create_db(conection):
+    with open("schema.sql", 'r') as f:
+        schema = f.read()
+        cur = conection.cursor()
+        conection.executescript(schema)
+
+def insertConnection(user, connected, disconnected, reason, ip):
     cur = db.cursor()
-    cur.execute("UPDATE nickname SET count = count + 1 WHERE user = ? AND nickname = ?", [id, nickname])
+    cur.execute("INSERT INTO connection (user, connected, disconnected, reason, ip) " + \
+                "VALUES (?, ?, ?, ?, ?)", \
+                [user, connected, disconnected, reason, "0.0.0.0" if hideIp else ip] \
+                )
+
+def insertUser(id):
+    logging.debug("Creating new user " + str(id))
+    cur = db.cursor()
+    cur.execute("INSERT INTO user (id) VALUES (?)", [id])
 
 def userExists(id):
     cur = db.cursor()
@@ -181,5 +199,15 @@ def userExists(id):
         return True
     return False
 
-if __name__ == "__main__":
+def nicknameUsed(id, nickname):
+    cur = db.cursor()
+    cur.execute("UPDATE nickname SET count = count + 1 WHERE user = ? AND nickname = ?", [id, nickname])
+
+def insertLog(filename):
+    cur = db.cursor()
+    cur.execute("INSERT INTO log (name) VALUES (?)", [filename])
+    print(cur.lastrowid)
+    return cur.lastrowid
+
+if __name__ == '__main__':
     main()
