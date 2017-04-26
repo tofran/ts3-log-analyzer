092e49b13e243cfec4f94c7fecc374c964514261
Francisco Marques <kiko96kiko@gmail.com>
2017-04-26 01:13:47 +0100

    Added options for creating links

--- ts3stats_lib.php
+++ ts3stats_lib.php
@@ -2,37 +2,55 @@
 $DATABASE_PATH = 'ts3stats.db';
 
 $db = NULL;
-try {
-	$db = new PDO('sqlite:' . $DATABASE_PATH);
-	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	die($e->getMessage());
+setup();
+
+function setup(){
+	global $DATABASE_PATH, $db;
+	try {
+		$db = new PDO('sqlite:' . $DATABASE_PATH);
+		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //ERRMODE_SILENT ; ERRMODE_EXCEPTION
+	} catch (PDOException $e) {
+		die();
+	}
 }
 
-function executeQuerry($query, $params = array()){
+function executeQuerry($query, $params = array(), $single = false){
 	global $db;
     $stmt = $db->prepare($query);
 	$stmt->execute($params);
+	if($single){
+		return $stmt;
+	}
 	return $stmt->fetchAll();
 }
 
-function ouputTable($query, $header = true){
+function ouputTable($query, $params = array(), $links = array(), $header = true){
 	echo "<table>";
-	$result = executeQuerry($query);
+	$result = executeQuerry($query, $params);
 	foreach ($result as $row) {
 		if($header){
 			echo "<tr>";
 			foreach ($row as $col => $value){
-				echo "<th>" . $col . '</th>';
+				echo "<th>" . htmlspecialchars($col, ENT_QUOTES, 'UTF-8') . '</th>';
 			}
 			echo "</tr>";
 			$header = false;
 		}
 
 		echo "<tr>";
+		$item = 1;
+
 		foreach ($row as $col => $value){
-			echo "<td>" . $value . '</td>';
+			echo "<td>";
+			if (isset($links[$item])) { //
+				echo str_replace('%', $value, $links[$item]);
+			}
+			else{
+				echo $value;
+			}
+			echo '</td>';
+			$item++;
 		}
 		echo "</tr>";
 	}		
